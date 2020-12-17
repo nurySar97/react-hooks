2bd43aabefa62f16b7a6c5b98c5dc7c3afb5ce97
Muhammet <muhammet@affexgroup.com>
2020-12-17 19:48:22 +0200

    useRef

--- src/components/useRef.js
+++ src/components/useRef.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+
 
 const UseRef = () => {
+    const [value, setValue] = useState('initial')
+    let renderCount = useRef(0)
+    let setFocusToInput = useRef(null)
+
+    let prevValue = useRef('initial')
+    useEffect(() => {
+        ++renderCount.current
+        setFocusToInput.current.focus()
+    })
+
+    useEffect(() => {
+        prevValue.current = value
+    }, [value])
     return (
-        <div className='col-12 bg-warning mt-5'>
-            Hello UseRef
+        <div className='col-12 bg-warning mt-5 p-4 text-light'>
+            <h1>Count of Renders {renderCount.current}</h1>
+            <h2>Previous Value: {prevValue.current}</h2>
+            <input
+                ref={setFocusToInput}
+                className='form-control'
+                type="text" value={value}
+                onChange={e => setValue(e.target.value)}
+            />
+            <button className="btn btn-success mt-3" onClick={() => setFocusToInput.current.focus()}>Set Focus To Input</button>
         </div>
     )
 }
