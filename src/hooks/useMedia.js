2d72787d4a67e7793db40c2178f43798b4e3b1c6
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-04 03:07:23 +0200

    useMedia was developed

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -1,54 +1,41 @@
 import { useState, useCallback, useEffect } from "react";
-let { matchMedia } = window;
-// 320
-const smallSmMedia = matchMedia("(min-width: 0) and (max-width: 320px)");
-// 375
-const smallLgMedia = matchMedia(
-  "(min-width: 320.9px) and (max-width: 375.9px)"
-);
-// 425
-const mediumSmMedia = matchMedia(
-  "(min-width: 375.9px) and (max-width: 425.9px)"
-);
-// 768
-const mediumLgMedia = matchMedia(
-  "(min-width: 425.9px) and (max-width: 768.9px)"
-);
-// 1024
-const largeSmMedia = matchMedia(
-  "(min-width: 768.9px) and (max-width: 1024.9px)"
-);
-// 1440
-const largeLgMedia = matchMedia("(min-width: 1025px)");
+const { matchMedia } = window;
 
-const sizeStore = [
-  smallSmMedia,
-  smallLgMedia,
-  mediumSmMedia,
-  mediumLgMedia,
-  largeSmMedia,
-  largeLgMedia,
+const mediaMatchSizes = [
+  "(min-width: 0) and (max-width: 320px)",
+  "(min-width: 320.9px) and (max-width: 375.9px)",
+  "(min-width: 375.9px) and (max-width: 425.9px)",
+  "(min-width: 425.9px) and (max-width: 768.9px)",
+  "(min-width: 768.9px) and (max-width: 1024.9px)",
+  "(min-width: 1025px)",
+].map(matchMedia);
+
+const mediaMatchKeys = [
+  "isSmallSm",
+  "isSmallLg",
+  "isMediumSm",
+  "isMediumLg",
+  "isLargeSm",
+  "isLargeLg",
 ];
 
+// Get Screen Sizes
+const getMediaMatches = () => {
+  return mediaMatchKeys.reduce((acc, value, index) => {
+    acc[`${value}Media`] = mediaMatchSizes[index].matches;
+    return acc;
+  }, {});
+};
+
 const subscribe = (handler) => {
-  sizeStore.forEach((media) => media.addEventListener("change", handler));
+  mediaMatchSizes.forEach((media) => media.addEventListener("change", handler));
 };
 
 const unsubscribe = (handler) => {
-  sizeStore.forEach((media) => media.removeEventListener("change", handler));
+  mediaMatchSizes.forEach((media) =>
+    media.removeEventListener("change", handler)
+  );
 };
-
-// Get Screen Sizes
-function getMediaMatches() {
-  return {
-    isSmallSmMedia: smallSmMedia.matches,
-    isSmallLgMedia: smallLgMedia.matches,
-    isMediumSmMedia: mediumSmMedia.matches,
-    isMediumLgMedia: mediumLgMedia.matches,
-    isLargeSmMedia: largeSmMedia.matches,
-    isLargeLgMedia: largeLgMedia.matches,
-  };
-}
 
 const Default = () => {
   const [mediaMatches, setMediaMatches] = useState(getMediaMatches());
